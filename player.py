08ef5927d507b653e77326c5e887500e78773edb
Ulquiorra <153022510+ulquiorraexe@users.noreply.github.com>
2025-04-07 19:38:30 +0300

    Update player.py

--- player.py
+++ player.py
@@ -3,15 +3,17 @@ STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-
 class Player(Turtle):
-
     def __init__(self):
         super().__init__()
         self.shape("turtle")
-        self.pu()
-        self.color("black")
-        self.goto(STARTING_POSITION)
+        self.color("green")
+        self.penup()
+        self.goto(0, -250)
+        self.setheading(90)
 
     def up(self):
-        self.fd(10)
+        self.forward(10)
+
+    def reset_position(self):
+        self.goto(0, -250)
