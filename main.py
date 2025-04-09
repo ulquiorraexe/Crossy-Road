05ae9604e7982a0fbbe500b43ff045803c843d86
Ulquiorra <153022510+ulquiorraexe@users.noreply.github.com>
2025-04-09 16:59:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,32 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-cem = Player()
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    screen.listen()
-    screen.onkey(key="Up", fun=cem.up)
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Check for collision with cars
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    # Check if the player has reached the finish line
+    if player.ycor() > 280:
+        player.reset_position()
+        car_manager.increase_speed()
+        scoreboard.increase_score()
+        
+screen.exitonclick()
